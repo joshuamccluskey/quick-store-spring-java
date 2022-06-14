f4db0b7188d37ad49e353587b9bbf3881a29868c
joshuamccluskey <jpiff57@gmail.com>
2022-06-14 12:15:16 -0700

    Inital commit for quick store Sping Intlzr package

---
+++ src/main/java/com/joshuamccluskey/quickstorespringjava/QuickStoreSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joshuamccluskey.quickstorespringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStoreSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStoreSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshuamccluskey/quickstorespringjava/QuickStoreSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshuamccluskey.quickstorespringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStoreSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
