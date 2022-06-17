31f89d6a1f3ea6cbca46210b09221f2f564baa67
joshuamccluskey <jpiff57@gmail.com>
2022-06-17 09:55:32 -0700

    Updated Model schema of product

--- src/main/java/com/joshuamccluskey/quickstorespringjava/Product.java
+++ src/main/java/com/joshuamccluskey/quickstorespringjava/Product.java
@@ -1,7 +1,13 @@
 package com.joshuamccluskey.quickstorespringjava;
 
 
+
 public class Product {
     private String name;
+    private String brand;
+    private Integer price;
+    private String origin;
+    private Integer qty;
+    private String imageURL;
 
 }
