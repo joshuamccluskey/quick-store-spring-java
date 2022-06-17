535bc693852d2b0f831796aae8b516304c2279ef
joshuamccluskey <jpiff57@gmail.com>
2022-06-17 09:57:27 -0700

    Implemented gettter and setter to product and constructor

--- src/main/java/com/joshuamccluskey/quickstorespringjava/Product.java
+++ src/main/java/com/joshuamccluskey/quickstorespringjava/Product.java
@@ -3,11 +3,74 @@ package com.joshuamccluskey.quickstorespringjava;
 
 
 public class Product {
+
     private String name;
     private String brand;
     private Integer price;
     private String origin;
     private Integer qty;
     private String imageURL;
+
+    public Product(String name, String brand, Integer price, String origin, Integer qty, String imageURL) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.origin = origin;
+        this.qty = qty;
+        this.imageURL = imageURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
+
+
+
 
 }
